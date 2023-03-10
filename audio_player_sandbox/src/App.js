e0f54ec3643333f6a34751d416676cb2307edea5
unknown <justinchervony@gmail.com>
2023-03-09 19:33:03 -0500

    Initial commit and some basic component setup. Added assets and gitignored.

---
+++ audio_player_sandbox/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+const audioElement = new Audio();
+
+const tracks = [
+  {
+    title: 'Africa',
+    artist: 'Toto',
+    audioSrc: 'string',
+		image: 'string',
+    color: 'red',
+  },
+  {
+    title: 'Rubberband Man',
+    artist: 'The Spinners',
+    audioSrc: 'string',
+		image: 'string',
+    color: 'green',
+  },
+  {
+    title: 'Titanium',
+    artist: 'David Guetta',
+    audioSrc: 'string',
+		image: 'string',
+    color: 'blue',
+  }
+]
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ audio_player_sandbox/src/tracks.js
@@ -0,0 +1,25 @@
+
+
+export default [
+    {
+        title: 'Africa',
+        artist: 'Toto',
+        audioSrc: './assets/03_Africa_5n7RDxe.m4a',
+        image: './assets/Toto-IV-album-cover.jpg',
+        color: 'red',
+    },
+    {
+        title: 'Rubberband Man',
+        artist: 'The Spinners',
+        audioSrc: './assets/ytmp3free.cc_the-rubberband-man-youtubemp3free.org.mp3',
+        image: './assets/spinners.jpeg',
+        color: 'green',
+    },
+    {
+        title: 'The Funeral',
+        artist: 'Band of Horses',
+        audioSrc: './assets/04_The_Funeral.m4a',
+        image: './assets/band of horses.jpeg',
+        color: 'blue',
+    }
+  ];
